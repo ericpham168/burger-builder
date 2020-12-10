fc89f528998d979b228a25c67332006bd6af778e
small <ericpham168@gmail.com>
2020-12-10 17:24:33 +0700

    initiate orderform

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,46 +4,92 @@ import classes from './ContactData.module.css'
 import * as actionCreator from '../../../containers/store/actions/index'
 import { connect } from 'react-redux'
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
-        name: "",
-        email: "",
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: "your name"
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: "your street"
+                },
+                value: ''
+            },
+            phoneNumb: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: "your mobilePhone"
+                },
+                value: ''
+            },
+            age: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: '19', displayValue: '19' },
+                        { value: '20', displayValue: '20' },
+                        { value: '21', displayValue: '21' },
+                        { value: '22', displayValue: '22' },
+                    ]
+                },
+                value: ''
+            },
         }
     }
 
     orderHanler = (event) => {
         event.preventDefault();
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredient: this.props.ings,
             price: this.props.totalPrice,
-            customer: {
-                name: 'Small',
-                address: {
-                    street: 'binh thanh',
-                    phoneNum: '0782121999',
-                },
-                age: '22'
-            },
-            dilivery: 'tiki'
+            customer: formData
         }
         this.props.onSendOrder(order);
     }
 
     renderBody() {
+        const formElementArr = [];
+        for (let key in this.state.orderForm) {
+            formElementArr.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
+
         return this.props.loading ? <Spinner /> : <div className={classes.contactData}>
             <h3>Enter your contact data</h3>
-            <form>
-                <input type="text" name="Name" placeholder="Your name"></input>
-                <input type="text" name="Email" placeholder="Your email"></input>
-                <input type="text" name="Street" placeholder="Your street"></input>
-                <input type="text" name="PostalCode" placeholder="Your PostalCode"></input>
+            <form onSubmit={this.orderHanler}>
+                {formElementArr.map(formElement => (
+                    <Input Key={formElement.id} elementType={formElement.config.elementType}
+                        changed={(event) => this.inputChangedhandler(event, formElement.id)} elementConfig={formElement.config.elementConfig} value={formElement.config.value}></Input>
+                ))}
+                <Button key="btnOrder" btnType="Success">ORDER</Button>
             </form>
-            <Button btnType="Success" clicked={this.orderHanler}>ORDER</Button>
-        </div>
+        </div >
+    }
+
+    inputChangedhandler = (event, inputIdentifier) => {
+        const updatedOrderForm = { ...this.state.orderForm };
+        const updateFormElement = { ...updatedOrderForm[inputIdentifier] };
+        updateFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updateFormElement;
+        this.setState({ orderForm: updatedOrderForm });
     }
 
 
